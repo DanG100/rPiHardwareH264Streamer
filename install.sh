937352cc6bfa313d0dba8d400f18d9f3e7630047
DanG100 <DanG100@users.noreply.github.com>
2020-02-22 18:23:57 -0800

    install improvements

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install socat
 sudo rpi-update
+sudo raspi-config nonint do_camera 0
+sudo usermod -aG netdev $user
 
 chmod a+x ./camServer.sh
 
